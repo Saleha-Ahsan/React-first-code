3812c30a9768793fb154f49827e20df89e2f8026
Saleha Ahsan <salehaahsan89@gmail.com>
2025-09-30 23:43:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    React.createElement("h1", null, "Hello, world!"),
+    // React.appendChild("p", null, "This is a simple React application.")
+);
