a6492086b89843e1da72d4c4ca332656f8fedc5e
Saleha Ahsan <salehaahsan89@gmail.com>
2025-10-01 15:07:06 +0500

    Add files via upload
    2nd file unorder and order list using react, not uploading main.jsx

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  document.getElementById("root");
+  return(
+    <div>
+    <h1>hello react</h1>
+    <ul>
+      <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos tempora libero quaerat!</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, minus id enim at sint iure.</li>
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, quis ex.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis!</li>
+      <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed explicabo ipsam enim quo at necessitatibus aliquam fuga! Provident, magnam numquam.</li>
+    </ul>
+    <h1>hello react</h1>
+    <ol>
+      <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos tempora libero quaerat!</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, minus id enim at sint iure.</li>
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, quis ex.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis!</li>
+      <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed explicabo ipsam enim quo at necessitatibus aliquam fuga! Provident, magnam numquam.</li>
+    </ol>
+    </div>
+  )
+}
+
+export default App
